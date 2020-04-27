6f0f0fb1c134e8630491b1761c7f63700bb0581b
drodriguez0117 <drodriguez.0117@gmail.com>
2020-04-26 21:25:07 -0400

    Configure ActiveStorage

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_19_024008) do
+ActiveRecord::Schema.define(version: 2020_04_22_022433) do
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "listing_types", force: :cascade do |t|
     t.string "name"
     t.text "description"
-    t.boolean "is_active"
+    t.boolean "active"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -24,9 +45,10 @@ ActiveRecord::Schema.define(version: 2020_04_19_024008) do
     t.string "title"
     t.string "description"
     t.integer "listing_type_id"
-    t.boolean "is_active"
+    t.integer "media_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 end

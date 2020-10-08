d29b1ec52fd53db636c0b0a9c401f9d3fdf73c8e
drodriguez0117 <drodriguez.0117@gmail.com>
2020-10-07 20:36:25 -0400

    modify seeds with images
    update seeds.rb to attach image to listing
    for testing purposes

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,17 +9,28 @@
 #
 
 5.times do
-  user = User.create(email: Faker::Internet.email, password_digest: Faker::Internet.password)
+  user = User.create(email: Faker::Internet.email,
+                     password_digest: BCrypt::Password.create('test'))
 
   3.times do
-    user.listings.create(title: Faker::Appliance.equipment,
+    v = user.listings.create(title: Faker::Appliance.equipment,
                          description: Faker::Lorem.sentence(word_count: 8),
                          listing_type: 'offering')
+
+    file_path = 'spec/fixtures/files/melvin.jpg'
+    v.images.attach(io: File.open(file_path),
+                         filename: 'melvin.jpg',
+                         content_type: 'image/jpeg')
   end
 
   2.times do
-    user.listings.create(title: Faker::Appliance.equipment,
+    g = user.listings.create(title: Faker::Appliance.equipment,
                          description: Faker::Appliance.equipment,
                          listing_type: 'request')
+
+    #file_path = 'spec/fixtures/files/melvin.jpg'
+    #g.images.attach(io: File.open(file_path),
+    #                filename: 'melvin.jpg',
+    #                content_type: 'image/jpeg')
   end
 end

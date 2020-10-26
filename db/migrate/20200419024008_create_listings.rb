5429df06986b6316d0823f0d706d8d260753b3b0
drodriguez0117 <drodriguez.0117@gmail.com>
2020-10-25 20:20:27 -0400

    add limits to listings
    add length limit for title to 30
    add length limit for description to 200
    add validates_length_of in listing model
    create model tests

--- db/migrate/20200419024008_create_listings.rb
+++ db/migrate/20200419024008_create_listings.rb
@@ -1,8 +1,8 @@
 class CreateListings < ActiveRecord::Migration[6.0]
   def change
     create_table :listings do |t|
-      t.string :title, null: false
-      t.string :description
+      t.string :title, limit: 30, null: false
+      t.string :description, limit: 200
       t.string :listing_type, null: false
       t.boolean :is_active, null: false, default: true
       t.references :user, { null: false, foreign_key: true }
--- spec/models/listing_spec.rb
+++ spec/models/listing_spec.rb
@@ -14,6 +14,47 @@ RSpec.describe Listing, type: :model do
     it { should fail_with_null(:title) }
     it { should fail_with_null(:listing_type) }
 
+    it 'is valid with title length less than thirty chars' do
+      title = 'short title'
+      expect(Listing.new(FactoryBot.attributes_for(:listing,
+                                                   title: title,
+                                                   user: user))).to be_valid
+    end
+
+    it 'is not valid with title length greater than thirty chars' do
+      title = 'long title for testing the length.'
+      expect(Listing.new(FactoryBot.attributes_for(:listing,
+                                                   title: title,
+                                                   user: user))).not_to be_valid
+    end
+
+    it 'is valid with description less than 200 chars' do
+
+      description = 'NEW: Added default "Header row fixed" setting
+        NEW: Support "Comment with line comment" #247
+				FIX: "Value coloring" change not applied to open files'
+
+      expect(Listing.new(FactoryBot.attributes_for(:listing,
+                                                   description: description,
+                                                   images: :with_image,
+                                                   user: user))).to be_valid
+    end
+
+    it 'is not valid with description greater than 200 chars' do
+
+      description = 'NEW: Added default "Header row fixed" setting
+				NEW: Support "Comment with line comment" #247
+				FIX: "Value coloring" change not applied to open files
+        Customize plugin settings: Editor/General, Color Scheme, Formatting
+				Visit the CSV Plugin GitHub to read more about the available features & settings,
+        submit issues & feature request, or show your support by rating this plugin'
+
+      expect(Listing.new(FactoryBot.attributes_for(:listing,
+                                                   description: description,
+                                                   images: :with_image,
+                                                   user: user))).not_to be_valid
+    end
+
     it 'is valid with valid attributes' do
       expect(Listing.new(FactoryBot.attributes_for(:listing, user: user))).to be_valid
     end
